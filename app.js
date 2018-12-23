3af8db65523c2b66787e2d6c5c40996e78b1176d
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-12-23 13:24:49 +0200

    mongoose installed

--- app.js
+++ app.js
@@ -33,4 +33,30 @@ logger.on('message', (message) => {
   console.log('New message:', message);
 });
 
-logger.log('New Message');
+// logger.log('New Message');
+
+// ========= HTTP ===========
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello world!');
+    res.end();
+  } else if (req.url === '/api/courses') {
+    const courses = [1, 2, 3];
+    res.write(JSON.stringify(courses));
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write('Not found');
+    res.end();
+  }
+});
+
+server.listen(3000, () => console.log('Listening...'));
+
+// ==== other packages ===
+const _ = require('lodash');
+
+const array = _.partition([1,2,3,4,5,6], n => n % 2 === 0);
+// console.log(array); // [ [ 2, 4, 6 ], [ 1, 3, 5 ] ]
