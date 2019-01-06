0125894133db0c40198dbbbcb8f26d709074791f
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-06 16:26:16 +0200

    task with Vidly app done

--- vidly/server.js
+++ vidly/server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const Joi = require('joi');
 
 const app = express();
 
@@ -22,12 +23,15 @@ app.get('/api/genres', (req, res) => {
 app.get('/api/genres/:id', (req, res) => {
   const genre = genres.find(g => g.id === parseInt(req.params.id));
 
-  if (!genre) return res.send('Such genre was not found');
+  if (!genre) return res.status(404).send('Such genre was not found');
 
   res.send(genre);
 });
 
 app.post('/api/genres', (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const newGenre = {
     id: genres.length + 1,
     name: req.body.name
@@ -41,8 +45,11 @@ app.post('/api/genres', (req, res) => {
 app.put('/api/genres/:id', (req, res) => {
   const genre = genres.find(g => g.id === parseInt(req.params.id));
 
-  if (!genre) return res.send('Such genre was not found');
+  if (!genre) return res.status(404).send('Such genre was not found');
 
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  
   genre.name = req.body.name;
 
   res.send(genre);
@@ -51,7 +58,7 @@ app.put('/api/genres/:id', (req, res) => {
 app.delete('/api/genres/:id', (req, res) => {
   const genre = genres.find(g => g.id === parseInt(req.params.id));
 
-  if (!genre) return res.send('Such genre was not found');
+  if (!genre) return res.status(404).send('Such genre was not found');
 
   genres = genres.filter(g => g !== genre);
 
@@ -62,3 +69,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('Listening on 3000');
 });
+
+function validate(body) {
+  const schema = {
+    name: Joi.string().min(2).required()
+  }
+
+  return Joi.validate(body, schema);
+}
