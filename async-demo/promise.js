c3d25bcf6011ef14e000a2054daf87183e18f538
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-01-17 23:30:41 +0200

    promise work

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -10,3 +10,9 @@ let p = new Promise((resolve, reject) => {
 p
   .then(console.log)
   .catch(err => console.log('Error:', err.message));
+
+  // ==== Promise API ====
+Promise.resolve(1)
+  .then(result => console.log(result));
+Promise.reject(new Error('Reason for rejection...'))
+  .catch(err => console.log(err));
