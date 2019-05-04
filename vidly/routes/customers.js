d6b0774594e197c8e75fc596491aaa4b9fbf86d8
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-05-05 00:31:46 +0300

    customers post and get done

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const routes = express.Router();
+const router = express.Router();
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
@@ -13,3 +13,24 @@ const Customer = mongoose.model('Customer', new mongoose.Schema({
   }
 }));
 
+// === CREATE ===
+router.post('/', async (req, res) => {
+  let customer = new Customer({
+    _id: new mongoose.Types.ObjectId,
+    name: req.body.name
+  });
+
+  customer = await customer.save();
+  console.log(customer);
+
+  res.send(customer);
+});
+
+// === READ ===
+router.get('/', async (req, res) => {
+  const customers = await Customer.find();
+
+  res.send(customers);
+});
+
+module.exports = router;
