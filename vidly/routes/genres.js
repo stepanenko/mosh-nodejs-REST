ba70aa32a2a69e0884526bef3b3c31fbc36cf380
stepanenko <serhiy.stepanenko@aiesec.net>
2019-05-22 15:21:56 +0300

    genre model created

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,23 +1,14 @@
 
-const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-  name: {
-    type: 'String',
-    required: true,
-    minlength: 3,
-    maxlength: 30
-  }
-}));
+const { Genre, validate } = require('../models/genre');
 
 
 // =====  CREATE  =====
 
 router.post('/', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({
@@ -61,7 +52,7 @@ router.get('/:id', async (req, res) => {
 // =====  UPDATE  =====
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   
   // === Query-First Approach (my version): ===
@@ -108,17 +99,6 @@ router.delete('/:id', async (req, res) => {
   console.log(`Genre '${genre.name}' was removed`);
   res.send(genre);
 });
-
-
-// ===== VALIDATION  =====
-
-function validateGenre(body) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
-
-  return Joi.validate(body, schema);
-}
 
 
 module.exports = router;
