a6295923b319ba5f59c77fff2ce99e9402f4167b
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-06-21 22:14:50 +0300

    population done

--- vidly/population.js
+++ vidly/population.js
@@ -11,11 +11,19 @@ const Author = mongoose.model('Author', new mongoose.Schema({
   website: String
 }));
 
+const Category = mongoose.model('Category', new mongoose.Schema({
+  name: String
+}));
+
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Author'
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
   }
 }));
 
@@ -30,10 +38,20 @@ async function createAuthor(name, bio, website) {
   console.log(result);
 }
 
-async function createCourse(name, author) {
+async function createCategory(name) {
+  const category = new Category({
+    name
+  });
+
+  const result = await category.save();
+  console.log(result);
+}
+
+async function createCourse(name, author, category) {
   const course = new Course({
     name,
-    author
+    author,
+    category
   });
 
   const result = await course.save();
@@ -43,11 +61,14 @@ async function createCourse(name, author) {
 async function listCourses() {
   const courses = await Course
     .find()
+    .populate('author', 'name -_id')
+    .populate('category', 'name -_id')
     .select('name author -_id');
 
   console.log(courses);
 }
 
 // createAuthor('Mosh', 'my bio', 'my website');
-// createCourse('Node Course', '5cf3d4e5e2587d1e20a7032f');
+// createCourse('Node Course', '5cf3d4e5e2587d1e20a7032f', '5d0d23088e2b040d8c4c9503');
+// createCategory('Back End');
 listCourses();
