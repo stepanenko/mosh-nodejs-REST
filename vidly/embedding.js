593c5c820f2e5fa7889bb7f5ef325ead37de0d0b
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-06-28 23:49:43 +0300

    start embedding

---
+++ vidly/embedding.js
@@ -0,0 +1,40 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://sergio:333444@stepser-komby.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+  .then(() => console.log('Connected to mongoDB'))
+  .catch(err => console.error('Couldnt connect to DB'));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: authorSchema
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course
+    .find()
+    .select('name author -_id');
+
+  console.log(courses);
+}
+
+// createCourse('Node Course', new Author({ name: 'Mosh' }));
+listCourses();
