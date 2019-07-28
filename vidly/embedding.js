af4ebaaa4bd1c1bb7ededc355a7512ce0600b449
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-07-28 21:06:55 +0300

    embedding docs - done

--- vidly/embedding.js
+++ vidly/embedding.js
@@ -31,10 +31,29 @@ async function createCourse(name, author) {
 async function listCourses() {
   const courses = await Course
     .find()
-    .select('name author -_id');
+    .select('name author _id');
 
   console.log(courses);
 }
 
+async function updateCourse(id) {
+  // const course = await Course.findById(id);
+  // course.author.name = 'Serhiy';
+  // course.save();
+
+  // Direct updating
+  await Course.updateOne({ _id: id}, {
+    $set: {
+      'author.name': 'Tom Cruise'
+    }
+    // To delete subdocument use $unset:
+    // $unset: {
+    //   'author': ''
+    // }
+  });
+}
+
 // createCourse('Node Course', new Author({ name: 'Mosh' }));
+updateCourse('5d167c5a7e62c219dc286b3f');
 listCourses();
+
