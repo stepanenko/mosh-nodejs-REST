4250689609042ccc54e1b4ec9dae3e2f066369db
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-08-01 11:43:25 +0300

    array of subdocs

--- vidly/embedding.js
+++ vidly/embedding.js
@@ -8,30 +8,44 @@ mongoose.connect('mongodb+srv://sergio:333444@stepser-komby.mongodb.net/test?ret
 const authorSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
+  website: String,
+  age: Number
 });
 
 const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: authorSchema
+  authors: [ authorSchema ]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name,
-    author
+    authors
   });
 
   const result = await course.save();
   console.log(result);
 }
 
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId);
+  course.authors.push(author);
+  course.save();
+}
+
+async function removeAuthor(courseId, authorId) {
+  const course = await Course.findById(courseId);
+  const author = course.authors.id(authorId);
+  author.remove();
+  course.save();
+}
+
 async function listCourses() {
   const courses = await Course
     .find()
-    .select('name author _id');
+    .select('name authors _id');
 
   console.log(courses);
 }
@@ -53,7 +67,12 @@ async function updateCourse(id) {
   });
 }
 
-// createCourse('Node Course', new Author({ name: 'Mosh' }));
-updateCourse('5d167c5a7e62c219dc286b3f');
+// createCourse('Java Course', [
+//   new Author({ name: 'Tom', website: 'tompro.com.ua' }),
+//   new Author({ name: 'Jess', bio: 'Junio' })
+// ]);
+// addAuthor('5d42a304c07b2c070481fa44', new Author({ name: 'Jim', age: 45 }));
+// removeAuthor('5d42a304c07b2c070481fa44', '5d42a49975283e21b8c3c5fb');
+// updateCourse('5d167c5a7e62c219dc286b3f');
 listCourses();
 
