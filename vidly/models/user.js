96a3eb45988be3be58144d2a68aa2a3a99cf869f
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-25 12:24:09 +0200

    added password complexity

--- vidly/models/user.js
+++ vidly/models/user.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const Joi = require('joi');
+const PasswordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -26,10 +27,20 @@ const userSchema = new mongoose.Schema({
 
 
 function validateUser(user) {
+  const complexityOptions = {
+    min: 5,
+    max: 255,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 2,
+    symbol:1
+  }
+
   const schema = {
     name: Joi.string().min(3).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(5).max(255).required()
+    // password: Joi.string().min(5).max(255).required()
+    password: new PasswordComplexity(complexityOptions)
   }
 
   return Joi.validate(user, schema);
--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -16,12 +16,9 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered');
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
   await user.save();
+  
   res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
