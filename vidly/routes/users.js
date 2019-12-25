b8b229d1532b56818dfc9474c49bbcb4db4661b0
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-25 22:22:39 +0200

    hashing passwords

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,5 +1,6 @@
 
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 const { User, validate } = require('../models/user');
@@ -17,6 +18,8 @@ router.post('/', async (req, res) => {
   if (user) return res.status(400).send('User already registered');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
   
   res.send(_.pick(user, ['_id', 'name', 'email']));
